a181a23f90b74f1ef5d1b26d10506f7da574ffb6
BeastBalze <beastbalze111@gmail.com>
2024-06-25 00:52:46 +0530

    initial commit

---
+++ backend/config/db.config.js
@@ -0,0 +1,12 @@
+const mongoose = require('mongoose');
+exports.connectDb =  async () => {
+    // try {
+    //     await mongoose.connect("gfhjkl")
+    //     .then((con) => {console.log("gjhjk")})
+    //     .catch((err) => {console.log(err)});
+    // } catch (error) {
+    //     console.log(error);
+    // }
+
+    console.log("cxgfhjkl;")
+};
